05100ab9559a57cc94841c729ba3495b507778bd
mo1501 <mr.mo1501abdullahi@gmail.com>
2023-11-27 09:54:24 +0300

    added auth logic to backend

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ import path from "path";
 import dotenv from "dotenv"
 import { fileURLToPath } from "url";
 import { error } from "console";
+import authRoutes from "./routes/auth.js";
+import {register} from "./controllers/auth.js"
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -35,6 +37,12 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES*/
+app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use("auth", authRoutes);
 /* MONGOOSE SETUP */
 
 const PORT = process.env.PORT || 6001;
