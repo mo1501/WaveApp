bb732769b0f20c0e22b5cd7d210f529196d980fe
mo1501 <mr.mo1501abdullahi@gmail.com>
2023-11-27 10:55:51 +0300

    added post logic to backend

--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ import dotenv from "dotenv"
 import { fileURLToPath } from "url";
 import { error } from "console";
 import authRoutes from "./routes/auth.js";
-import {register} from "./controllers/auth.js"
+import userRoutes from "./routes/user.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js"
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -40,9 +44,13 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES*/
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
 
 /* ROUTES */
-app.use("auth", authRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes)
 /* MONGOOSE SETUP */
 
 const PORT = process.env.PORT || 6001;
