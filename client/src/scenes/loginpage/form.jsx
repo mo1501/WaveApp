ffbed905006e45792eb7d6eb5778636dded91a1d
mo1501 <mr.mo1501abdullahi@gmail.com>
2023-12-19 15:48:03 +0300

    modified fetch api

--- client/src/scenes/loginpage/form.jsx
+++ client/src/scenes/loginpage/form.jsx
@@ -64,7 +64,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "https://wave-app-backend.vercel.app:6001/auth/register",
+      "https://wave-app-backend.vercel.app/auth/register",
       {
         method: "POST",
         body: formData,
@@ -79,7 +79,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("https://wave-app-backend.vercel.app/", {
+    const loggedInResponse = await fetch("https://wave-app-backend.vercel.app/auth", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
