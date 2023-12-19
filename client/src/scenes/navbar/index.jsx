ec0898ec2cc397ded91a2fb692598f401d9eddf0
mo1501 <mr.mo1501abdullahi@gmail.com>
2023-12-19 14:26:03 +0300

    added vercel configurations

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Wave
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
