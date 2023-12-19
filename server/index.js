d348d71db36bb4d243be22865be8255290e41d4b
mo1501 <mr.mo1501abdullahi@gmail.com>
2023-12-19 14:51:58 +0300

    added cors rules

--- server/index.js
+++ server/index.js
@@ -30,6 +30,9 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use(cors({
+    origin: 'https://wave-app-frontend.vercel.app/'
+}));
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 /* FILE STORAGE */
