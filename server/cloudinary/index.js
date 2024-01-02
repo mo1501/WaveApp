5cab139ea0e8ddfb592102c4611112044270f505
mo1501 <mr.mo1501abdullahi@gmail.com>
2024-01-02 16:25:51 +0300

    added project files

---
+++ server/cloudinary/index.js
@@ -0,0 +1,22 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  folder: 'WaveApp',
+  params: {
+    folder: 'WaveApp',
+    allowedFormats: ['jpeg', 'png', 'jpg'],
+  },
+});
+
+export  {
+  cloudinary,
+  storage,
+};
