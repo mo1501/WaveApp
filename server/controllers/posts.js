58ec206c3414a74970429d63309a670c31e97078
mo1501 <mr.mo1501abdullahi@gmail.com>
2024-01-18 11:45:44 +0300

    debugging

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -6,9 +6,10 @@ import { cloudinary, storage } from "../cloudinary/index.js";
 /* CREATE */
 export const createPost = async (req, res) => {
     try {
-        const { userId, description, picturePath } = req.body;
+        const { userId, description } = req.body;
+        const picturePath = req.body.file;
         const user = await User.findById(userId);
-        console.log("req.body picturepath:", picturePath);
+        console.log("req.body.file -- picturepath:", picturePath);
         const b64 = Buffer.from(picturePath).toString("base64");
         let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
         console.log(`dataUrI -- ${dataURI}`);
