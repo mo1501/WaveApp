b8197ab26ad187b482791f4b3f6927a83585f60f
mo1501 <mr.mo1501abdullahi@gmail.com>
2024-01-18 09:53:12 +0300

    debugging

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -42,6 +42,7 @@ export const getFeedPosts = async (req, res) => {
     try {
         const post = await Post.find();
         res.status(200).json(post);
+        console.log(post);
     } catch (err) {
         res.status(404).json({ message: err.message });
     }
